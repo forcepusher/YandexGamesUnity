25e765b9a38eef1f094352124b3dd0c6033ecaae
forcepusher <bionitsoup@gmail.com>
2021-11-27 02:22:34 +0700

    Added StringBuffer test.

--- Packages/com.agava.yandexgames/Tests/Runtime/StringBufferTests.cs
+++ Packages/com.agava.yandexgames/Tests/Runtime/StringBufferTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
 using NUnit.Framework;
 
 namespace YandexGames.Utility.Tests
@@ -7,7 +10,19 @@ namespace YandexGames.Utility.Tests
         [Test]
         public void ShouldConvertString()
         {
-            //new StringBuffer()
+            string testString =
+                "henlo programmer\n" +
+                "helllo you STINKY PROGRAMMER\n" +
+                "go code in javascript ugly\n";
+
+            byte[] testStringBytes = Encoding.UTF8.GetBytes(testString);
+            IntPtr stringBufferPointer = Marshal.AllocHGlobal(testStringBytes.Length);
+            Marshal.Copy(testStringBytes, 0, stringBufferPointer, testStringBytes.Length);
+
+            var stringBuffer = new StringBuffer(stringBufferPointer, testStringBytes.Length);
+            Assert.AreEqual(testString, stringBuffer.ToString());
+
+            Marshal.FreeHGlobal(stringBufferPointer);
         }
     }
 }
