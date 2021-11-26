e21950f28f6f8f73a92567cf123dd30e3b72c01c
forcepusher <bionitsoup@gmail.com>
2021-11-26 22:05:15 +0700

    Copy-pasting for the VideoAd

--- Packages/com.agava.yandexgames/Runtime/VideoAd.cs
+++ Packages/com.agava.yandexgames/Runtime/VideoAd.cs
@@ -1,15 +1,79 @@
+using System;
 using System.Runtime.InteropServices;
+using System.Text;
+using AOT;
+using UnityEngine;
 
 namespace YandexGames
 {
-    public class VideoAd
+    /// <summary>
+    /// Proxy for ysdk.adv.showRewardedVideo() method in YandexGames SDK.
+    /// </summary>
+    /// <remarks>
+    /// Normally "VideoAd" class should not be static,
+    /// but a proxy class has to match the SDK logic.
+    /// </remarks>
+    public static class VideoAd
     {
-        public void Show()
+        // Mutable static fields. Disgusting.
+        private static Action s_onOpenCallback;
+        private static Action s_onRewardedCallback;
+        private static Action s_onCloseCallback;
+        private static Action<string> s_onErrorCallback;
+
+        public static void Show(Action onOpenCallback = null, Action onRewardedCallback = null,
+            Action onCloseCallback = null, Action<string> onErrorCallback = null)
         {
-            ShowVideoAd();
+            // You should be ashamed of yourself.
+            s_onOpenCallback = onOpenCallback;
+            s_onRewardedCallback = onRewardedCallback;
+            s_onCloseCallback = onCloseCallback;
+            s_onErrorCallback = onErrorCallback;
+
+            ShowVideoAd(OnOpenCallback, OnRewardedCallback, OnCloseCallback, OnErrorCallback);
         }
 
         [DllImport("__Internal")]
-        private static extern bool ShowVideoAd();
+        private static extern bool ShowVideoAd(Action openCallback, Action rewardedCallback, Action closeCallback, Action<IntPtr, int> errorCallback);
+
+        [MonoPInvokeCallback(typeof(Action))]
+        private static void OnOpenCallback()
+        {
+            if (YandexGamesSdk.CallbackLogging)
+                Debug.Log("OnOpenCallback invoked");
+
+            s_onOpenCallback?.Invoke();
+        }
+
+        [MonoPInvokeCallback(typeof(Action))]
+        private static void OnRewardedCallback()
+        {
+            if (YandexGamesSdk.CallbackLogging)
+                Debug.Log("OnRewardedCallback invoked");
+
+            s_onRewardedCallback?.Invoke();
+        }
+
+        [MonoPInvokeCallback(typeof(Action))]
+        private static void OnCloseCallback()
+        {
+            if (YandexGamesSdk.CallbackLogging)
+                Debug.Log("OnCloseCallback invoked");
+
+            s_onCloseCallback?.Invoke();
+        }
+
+        [MonoPInvokeCallback(typeof(Action<IntPtr, int>))]
+        private static void OnErrorCallback(IntPtr errorMessageBufferPtr, int errorMessageBufferLength)
+        {
+            byte[] errorMessageBuffer = new byte[errorMessageBufferLength];
+            Marshal.Copy(errorMessageBufferPtr, errorMessageBuffer, 0, errorMessageBufferLength);
+            string errorMessage = Encoding.UTF8.GetString(errorMessageBuffer);
+
+            if (YandexGamesSdk.CallbackLogging)
+                Debug.Log("OnErrorCallback invoked, errorMessage = " + errorMessage);
+
+            s_onErrorCallback?.Invoke(errorMessage);
+        }
     }
 }
