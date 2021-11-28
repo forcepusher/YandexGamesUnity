1e950b7cef30e40213254fdb1d31750f64ba1bca
forcepusher <bionitsoup@gmail.com>
2021-11-28 23:12:39 +0700

    Added label that would check for authorization

--- Packages/com.agava.yandexgames/Samples/Playtesting/PlaytestingCanvas.cs
+++ Packages/com.agava.yandexgames/Samples/Playtesting/PlaytestingCanvas.cs
@@ -1,12 +1,32 @@
+#pragma warning disable
+
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace YandexGames.Samples
 {
     public class PlaytestingCanvas : MonoBehaviour
     {
+        [SerializeField]
+        private Text _isAuthorizedText;
+
         private void Awake()
         {
             YandexGamesSdk.CallbackLogging = true;
+        }
+
+        private IEnumerator Start()
+        {
+#if !UNITY_WEBGL || UNITY_EDITOR
+            yield break;
+#endif
+
+            while (true)
+            {
+                yield return new WaitForSecondsRealtime(0.5f);
+                _isAuthorizedText.color = PlayerAccount.IsAuthorized ? Color.green : Color.red;
+            }
         }
 
         public void OnShowInterestialButtonClick()
