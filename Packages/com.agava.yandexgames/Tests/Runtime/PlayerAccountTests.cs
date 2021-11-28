fcf74cd7b71a8c325ea53f32c4b86693b147032a
forcepusher <bionitsoup@gmail.com>
2021-11-29 01:38:41 +0700

    Adding tests

---
+++ Packages/com.agava.yandexgames/Tests/Runtime/PlayerAccountTests.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace YandexGames.Tests
+{
+    public class PlayerAccountTests
+    {
+        [Test]
+        public void ShouldNotBeAuthorizedOnStart()
+        {
+            Assert.IsFalse(PlayerAccount.IsAuthorized);
+        }
+
+        [UnityTest]
+        public IEnumerator AuthenticateShouldInvokeErrorCallback()
+        {
+            bool callbackInvoked = false;
+            PlayerAccount.Authenticate(true, onErrorCallback: (message) => {
+                callbackInvoked = true;
+            });
+
+            yield return new WaitForSecondsRealtime(1);
+
+            Assert.IsTrue(callbackInvoked);
+        }
+    }
+}
