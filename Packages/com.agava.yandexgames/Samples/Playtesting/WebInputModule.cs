ba34fe3a642cdec27210109001da2d1d02ae9cf6
forcepusher <bionitsoup@gmail.com>
2021-11-30 13:54:32 +0700

    Debug code

---
+++ Packages/com.agava.yandexgames/Samples/Playtesting/WebInputModule.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WebInputModule : StandaloneInputModule
+{
+    //public override void UpdateModule()
+    //{
+    //    Debug.Log(nameof(UpdateModule));
+    //    base.UpdateModule();
+    //}
+
+    //public override bool IsModuleSupported()
+    //{
+    //    Debug.Log(nameof(IsModuleSupported));
+    //    return base.IsModuleSupported();
+    //}
+
+    //public override bool ShouldActivateModule()
+    //{
+    //    Debug.Log(nameof(ShouldActivateModule));
+    //    return base.ShouldActivateModule();
+    //}
+
+    //public override void ActivateModule()
+    //{
+    //    Debug.Log(nameof(ActivateModule));
+    //    base.ActivateModule();
+    //}
+
+    //public override void DeactivateModule()
+    //{
+    //    Debug.Log(nameof(DeactivateModule));
+    //    base.DeactivateModule();
+    //}
+
+    public override void Process()
+    {
+        if (!eventSystem.isFocused && ShouldIgnoreEventsOnNoFocus())
+            return;
+
+        bool usedEvent = SendUpdateEventToSelectedObject();
+
+        // case 1004066 - touch / mouse events should be processed before navigation events in case
+        // they change the current selected gameobject and the submit button is a touch / mouse button.
+
+        // touch needs to take precedence because of the mouse emulation layer
+        if (!ProcessTouchEvents() && input.mousePresent)
+            ProcessMouseEvent();
+
+        if (eventSystem.sendNavigationEvents)
+        {
+            if (!usedEvent)
+                usedEvent |= SendMoveEventToSelectedObject();
+
+            if (!usedEvent)
+                SendSubmitEventToSelectedObject();
+        }
+    }
+
+    private bool ProcessTouchEvents()
+    {
+        for (int i = 0; i < input.touchCount; ++i)
+        {
+            Touch touch = input.GetTouch(i);
+
+            if (touch.type == TouchType.Indirect)
+                continue;
+
+            bool released;
+            bool pressed;
+            var pointer = GetTouchPointerEventData(touch, out pressed, out released);
+
+            ProcessTouchPress(pointer, pressed, released);
+
+            if (!released)
+            {
+                ProcessMove(pointer);
+                ProcessDrag(pointer);
+            }
+            else
+                RemovePointerData(pointer);
+        }
+        return input.touchCount > 0;
+    }
+
+    private bool ShouldIgnoreEventsOnNoFocus()
+    {
+#if UNITY_EDITOR
+        return !UnityEditor.EditorApplication.isRemoteConnected;
+#else
+            return true;
+#endif
+    }
+}
