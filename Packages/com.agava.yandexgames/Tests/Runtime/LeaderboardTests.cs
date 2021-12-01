89fd4ae45c6175aef746277a620b33ca326a9af9
forcepusher <bionitsoup@gmail.com>
2021-12-01 17:45:16 +0700

    Updating leaderboard test

--- Packages/com.agava.yandexgames/Tests/Runtime/LeaderboardTests.cs
+++ Packages/com.agava.yandexgames/Tests/Runtime/LeaderboardTests.cs
@@ -7,16 +7,31 @@ namespace YandexGames.Tests
 {
     public class LeaderboardTests
     {
+        private const float InitializationTimeoutSeconds = 5;
+
         [UnitySetUp]
         public IEnumerator WaitForSdkInitialization()
         {
             yield return YandexGamesSdk.WaitForInitialization();
         }
 
-        [Test]
-        public void ShouldInitializeAutomatically()
+        [UnityTest]
+        public IEnumerator ShouldInitializeAutomatically()
         {
-            Assert.IsTrue(Leaderboard.IsInitialized);
+            float waitTime = 0;
+            while (waitTime <= InitializationTimeoutSeconds)
+            {
+                if (Leaderboard.IsInitialized)
+                {
+                    Assert.Pass($"Initialized in {waitTime} seconds.");
+                    yield break;
+                }
+
+                yield return null;
+                waitTime += Time.unscaledDeltaTime;
+            }
+
+            Assert.Fail($"Failed to initialize in {InitializationTimeoutSeconds} seconds.");
         }
 
         [Test]
