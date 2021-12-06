984563a6fd0abe98d497ac80a861b6161b364d21
forcepusher <bionitsoup@gmail.com>
2021-12-07 02:55:27 +0700

    Adding test for Json serialization and working around code stripping bug

--- Packages/com.agava.yandexgames/Runtime/Leaderboard/LeaderboardGetEntriesResponse.cs
+++ Packages/com.agava.yandexgames/Runtime/Leaderboard/LeaderboardGetEntriesResponse.cs
@@ -1,12 +1,16 @@
 using System;
+using UnityEngine.Scripting;
 
 namespace YandexGames
 {
     [Serializable]
     public class LeaderboardGetEntriesResponse
     {
+        [field: Preserve]
         public LeaderboardDescriptionResponse leaderboard;
+        [field: Preserve]
         public LeaderboardRangeResponse[] range;
+        [field: Preserve]
         public int userRank;
     }
 }
