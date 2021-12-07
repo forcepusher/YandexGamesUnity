ac9a2de23d29585a10f391c55d71cc357cbf2559
forcepusher <bionitsoup@gmail.com>
2021-12-07 19:59:44 +0700

    Quick testing setup

--- Packages/com.agava.yandexgames/Runtime/Leaderboard/LeaderboardDescriptionResponse.cs
+++ Packages/com.agava.yandexgames/Runtime/Leaderboard/LeaderboardDescriptionResponse.cs
@@ -1,10 +1,16 @@
 using System;
+using UnityEngine.Scripting;
+using UnityEngine.Serialization;
 
 namespace YandexGames
 {
     [Serializable]
     public class LeaderboardDescriptionResponse
     {
-
+        [field: Preserve]
+        public string appID;
+        // Well, there's nothing I can do about it lol.
+        //[field: Preserve]
+        //public bool default;
     }
 }
