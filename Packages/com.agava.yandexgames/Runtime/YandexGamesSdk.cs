307bb8cf5a43723b8f90c7f31e543b72494f9e8a
forcepusher <bionitsoup@gmail.com>
2021-12-11 23:23:17 +0700

    Tweaking summary

--- Packages/com.agava.yandexgames/Runtime/YandexGamesSdk.cs
+++ Packages/com.agava.yandexgames/Runtime/YandexGamesSdk.cs
@@ -25,7 +25,7 @@ namespace YandexGames
         private static extern bool Initialize();
 
         /// <summary>
-        /// Leaderboard is initialized automatically on load.
+        /// SDK is initialized automatically on load.
         /// If either something fails or called way too early, this will return false.
         /// </summary>
         public static bool Initialized => CheckSdkInitialization();
