6ce7d8d1b96819cf63280a6ba8a9595172636d3d
forcepusher <bionitsoup@gmail.com>
2021-12-12 16:38:33 +0700

    Using folders to group up the stuff.

--- Packages/com.agava.yandexgames/Runtime/Leaderboard/LeaderboardEntryResponse.cs
+++ Packages/com.agava.yandexgames/Runtime/Leaderboard/LeaderboardEntryResponse.cs
@@ -13,32 +13,8 @@ namespace YandexGames
         [field: Preserve]
         public int rank;
         [field: Preserve]
-        public Player player;
+        public PlayerAccountProfileDataResponse player;
         [field: Preserve]
         public string formattedScore;
-
-
-        [Serializable]
-        public class Player
-        {
-            [field: Preserve]
-            public string lang;
-            [field: Preserve]
-            public string publicName;
-            [field: Preserve]
-            public ScopePermissions scopePermissions;
-            [field: Preserve]
-            public string uniqueID;
-
-
-            [Serializable]
-            public class ScopePermissions
-            {
-                [field: Preserve]
-                public string avatar;
-                [field: Preserve]
-                public string public_name;
-            }
-        }
     }
 }
---
+++ Packages/com.agava.yandexgames/Runtime/PlayerAccount/PlayerAccountProfileDataResponse.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine.Scripting;
+
+namespace YandexGames
+{
+    [Serializable]
+    public class PlayerAccountProfileDataResponse
+    {
+        [field: Preserve]
+        public string lang;
+        [field: Preserve]
+        public string publicName;
+        [field: Preserve]
+        public ScopePermissions scopePermissions;
+        [field: Preserve]
+        public string uniqueID;
+
+
+        [Serializable]
+        public class ScopePermissions
+        {
+            [field: Preserve]
+            public string avatar;
+            [field: Preserve]
+            public string public_name;
+        }
+    }
+}
