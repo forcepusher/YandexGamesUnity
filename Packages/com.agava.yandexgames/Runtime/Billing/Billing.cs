9733882c2441e42739a7521bf7d4219442e195f3
forcepusher <bionitsoup@gmail.com>
2023-01-25 04:45:40 +0300

    Add billing purchase code

--- Packages/com.agava.yandexgames/Runtime/Billing/Billing.cs
+++ Packages/com.agava.yandexgames/Runtime/Billing/Billing.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Runtime.InteropServices;
+using AOT;
 using UnityEngine;
 
 namespace Agava.YandexGames
 {
     public static class Billing
     {
-        
+        public static void Purchase(string productId) => BillingPurchase(productId);
+
+        [DllImport("__Internal")]
+        private static extern void BillingPurchase(string productId);
     }
 }
