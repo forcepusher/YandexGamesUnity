70ed7f8b9f9f1c65d2b698efc1c5e76af843b9cd
forcepusher <bionitsoup@gmail.com>
2023-02-10 06:50:47 +0300

    Add code for images

---
+++ Packages/com.agava.yandexgames/Runtime/ProfilePicture/ProfilePicture.cs
@@ -0,0 +1,10 @@
+namespace Agava.YandexGames
+{
+    public static class ProfilePicture
+    {
+        public static void Fetch(string url, ProfilePictureSize size)
+        {
+
+        }
+    }
+}
