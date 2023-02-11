d07120c478b049d450485aa464b80c790868bb24
forcepusher <bionitsoup@gmail.com>
2023-02-11 08:51:20 +0300

    Work on remote image

--- Packages/com.agava.yandexgames/Runtime/ProfilePicture/ProfilePicture.cs
+++ Packages/com.agava.yandexgames/Runtime/ProfilePicture/ProfilePicture.cs
@@ -1,10 +1,14 @@
+using System;
+using System.Threading;
+using UnityEngine;
+
 namespace Agava.YandexGames
 {
     public static class ProfilePicture
     {
-        public static void Fetch(string url, ProfilePictureSize size)
+        public static void Fetch(string url, Action<Texture2D> successCallback = null, Action<string> errorCallback = null, ProfilePictureSize size = ProfilePictureSize.medium, CancellationToken cancellationToken = default)
         {
-
+            //RemoteImage
         }
     }
 }
--- Packages/com.agava.yandexgames/Runtime/Utility/RemoteImage.cs
+++ Packages/com.agava.yandexgames/Runtime/Utility/RemoteImage.cs
@@ -1,7 +1,42 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
 namespace Agava.YandexGames
 {
     public class RemoteImage
     {
+        private string _url;
 
+        public RemoteImage(string url)
+        {
+
+        }
+
+        //private IEnumerator DownloadAndSetProductImage(string imageUri)
+        //{
+        //    using (UnityWebRequest textureRequest = UnityWebRequestTexture.GetTexture(imageUri))
+        //    {
+        //        yield return textureRequest.SendWebRequest();
+        //        _productImage.texture = DownloadHandlerTexture.GetContent(textureRequest);
+        //    }
+        //}
+
+        public async void Download(string imageUrl, Action<Texture2D> successCallback = null, Action<string> errorCallback = null, CancellationToken cancellationToken = default)
+        {
+            using (UnityWebRequest downloadTextureWebRequest = UnityWebRequestTexture.GetTexture(imageUrl))
+            {
+                UnityWebRequestAsyncOperation downloadOperation = downloadTextureWebRequest.SendWebRequest();
+
+                while (!downloadOperation.isDone)
+                    await Task.Yield();
+
+                Texture2D texture = DownloadHandlerTexture.GetContent(downloadTextureWebRequest);
+
+                successCallback?.Invoke(texture);
+            }
+        }
     }
 }
