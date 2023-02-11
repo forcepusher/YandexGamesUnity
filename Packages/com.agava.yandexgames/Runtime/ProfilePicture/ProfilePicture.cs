4ef188c0ef288f53f86c9be8dbc09209267e6f14
forcepusher <bionitsoup@gmail.com>
2023-02-11 09:45:39 +0300

    Add download code

--- Packages/com.agava.yandexgames/Runtime/ProfilePicture/ProfilePicture.cs
+++ Packages/com.agava.yandexgames/Runtime/ProfilePicture/ProfilePicture.cs
@@ -6,9 +6,16 @@ namespace Agava.YandexGames
 {
     public static class ProfilePicture
     {
-        public static void Fetch(string url, Action<Texture2D> successCallback = null, Action<string> errorCallback = null, ProfilePictureSize size = ProfilePictureSize.medium, CancellationToken cancellationToken = default)
+        public static async void Fetch(string url, Action<Texture2D> successCallback = null, Action<string> errorCallback = null, ProfilePictureSize size = ProfilePictureSize.medium, CancellationToken cancellationToken = default)
         {
-            //RemoteImage
+            var profilePicture = new RemoteImage(url);
+
+            await profilePicture.Download();
+
+            if (profilePicture.IsDownloadSuccessful)
+                successCallback?.Invoke(profilePicture.Texture);
+            else
+                errorCallback?.Invoke(profilePicture.DownloadErrorMessage);
         }
     }
 }
--- Packages/com.agava.yandexgames/Runtime/Utility/RemoteImage.cs
+++ Packages/com.agava.yandexgames/Runtime/Utility/RemoteImage.cs
@@ -8,11 +8,52 @@ namespace Agava.YandexGames
 {
     public class RemoteImage
     {
-        private string _url;
+        private readonly string _url;
+
+        private Texture2D _texture;
+        public Texture2D Texture
+        {
+            get
+            {
+                if (!IsDownloadFinished)
+                    throw new InvalidOperationException($"Attempt to get {nameof(Texture)} while {nameof(IsDownloadFinished)} = {IsDownloadFinished}");
+
+                if (!IsDownloadSuccessful)
+                    throw new InvalidOperationException($"Attempt to get {nameof(Texture)} while {nameof(IsDownloadSuccessful)} = {IsDownloadSuccessful}");
+
+                return _texture;
+            }
+            private set
+            {
+                _texture = value;
+            }
+        }
+
+        public bool IsDownloadFinished { get; private set; }
+
+        public bool IsDownloadSuccessful { get; private set; }
+
+        public string DownloadErrorMessage { get; private set; }
 
         public RemoteImage(string url)
         {
+            _url= url;
+        }
 
+        public async Task Download(CancellationToken cancellationToken = default)
+        {
+            using (UnityWebRequest downloadTextureWebRequest = UnityWebRequestTexture.GetTexture(_url))
+            {
+                UnityWebRequestAsyncOperation downloadOperation = downloadTextureWebRequest.SendWebRequest();
+
+                while (!downloadOperation.isDone)
+                    await Task.Yield();
+
+                if (downloadOperation.webRequest.result != UnityWebRequest.Result.Success)
+                    DownloadErrorMessage = downloadOperation.webRequest.error;
+
+                Texture = DownloadHandlerTexture.GetContent(downloadTextureWebRequest);
+            }
         }
 
         //private IEnumerator DownloadAndSetProductImage(string imageUri)
@@ -23,20 +64,5 @@ namespace Agava.YandexGames
         //        _productImage.texture = DownloadHandlerTexture.GetContent(textureRequest);
         //    }
         //}
-
-        public async void Download(string imageUrl, Action<Texture2D> successCallback = null, Action<string> errorCallback = null, CancellationToken cancellationToken = default)
-        {
-            using (UnityWebRequest downloadTextureWebRequest = UnityWebRequestTexture.GetTexture(imageUrl))
-            {
-                UnityWebRequestAsyncOperation downloadOperation = downloadTextureWebRequest.SendWebRequest();
-
-                while (!downloadOperation.isDone)
-                    await Task.Yield();
-
-                Texture2D texture = DownloadHandlerTexture.GetContent(downloadTextureWebRequest);
-
-                successCallback?.Invoke(texture);
-            }
-        }
     }
 }
