5da5acc8ba4845d57452f043b7899d3e75108737
forcepusher <bionitsoup@gmail.com>
2023-04-06 22:32:33 +0700

    Add exploit protection

--- Packages/com.agava.yandexgames/Runtime/Advertising/VideoAd.cs
+++ Packages/com.agava.yandexgames/Runtime/Advertising/VideoAd.cs
@@ -16,6 +16,8 @@ namespace Agava.YandexGames
         private static Action s_onCloseCallback;
         private static Action<string> s_onErrorCallback;
 
+        private static bool s_isVideoAdOpen = false;
+
         /// <summary>
         /// Shows the rewarded video ad.
         /// </summary>
@@ -43,12 +45,20 @@ namespace Agava.YandexGames
             if (YandexGamesSdk.CallbackLogging)
                 Debug.Log($"{nameof(VideoAd)}.{nameof(OnOpenCallback)} invoked");
 
+            s_isVideoAdOpen = true;
+
             s_onOpenCallback?.Invoke();
         }
 
         [MonoPInvokeCallback(typeof(Action))]
         private static void OnRewardedCallback()
         {
+            if (!s_isVideoAdOpen)
+            {
+                Debug.Log($"Ignoring {nameof(VideoAd)}.{nameof(OnRewardedCallback)} because {nameof(s_isVideoAdOpen)} is {s_isVideoAdOpen}");
+                return;
+            }
+
             if (YandexGamesSdk.CallbackLogging)
                 Debug.Log($"{nameof(VideoAd)}.{nameof(OnRewardedCallback)} invoked");
 
@@ -60,6 +70,8 @@ namespace Agava.YandexGames
         {
             if (YandexGamesSdk.CallbackLogging)
                 Debug.Log($"{nameof(VideoAd)}.{nameof(OnCloseCallback)} invoked");
+
+            s_isVideoAdOpen = false;
 
             s_onCloseCallback?.Invoke();
         }
