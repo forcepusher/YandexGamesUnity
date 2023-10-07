072ce023f67f9a343f1ff37438b0781b723c8e05
forcepusher <bionitsoup@gmail.com>
2023-10-07 12:57:28 +0700

    Move review and shortcut to Tools folder

---
+++ Packages/com.agava.yandexgames/Runtime/Tools/ReviewPopup.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Agava.YandexGames
+{
+    public class ReviewPopup
+    {
+        public static void CanRequestReview(Action<bool> onResultCallback)
+        {
+            //s_onCanSuggestCallback = onResultCallback;
+
+            //ShortcutCanSuggestShortcut(CanSuggestShortcutCallback);
+        }
+    }
+}
